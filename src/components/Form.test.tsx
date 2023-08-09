39957782dccf9a6f3273ba84b48bdcf8fe97ad62
carlosavellar <carlos.avellar@gmail.com>
2023-08-08 22:25:08 -0300

    Testing user click

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Form from './Form';
 import { screen, render } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { IUser } from '../Interface/IUser';
 
 test('Testing length form elements', () => {
   render(<Form onAddUser={() => console.log('e')} />);
@@ -10,4 +12,30 @@ test('Testing length form elements', () => {
 
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
+});
+
+test('Testing user inputs', () => {
+  const argList: Array<IUser> = [];
+  const callBack = (users: IUser) => {
+    argList.push(users);
+  };
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  render(<Form onAddUser={callBack} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+  user.click(nameInput);
+  user.keyboard('Deusa Athena');
+
+  user.click(emailInput);
+  user.keyboard('athena@gmail.com');
+
+  const button = screen.getByRole('button');
+  user.click(button);
+
+  expect(argList).toHaveLength(1);
+  expect(argList).toEqual({
+    name: 'Deusa Athena',
+    email: 'athena@gmail.com',
+  });
 });
